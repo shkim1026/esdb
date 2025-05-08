c6f3a6d8ac01bbe86c89426406b6cf2ea7e6a598
shkim1026 <60453195+shkim1026@users.noreply.github.com>
2025-05-08 14:20:07 -0700

    Moves apikeys to .env

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD3MgroGV7tj6Spb2VdadYR4dcAgGY_pww",
-  authDomain: "esdb-4bc35.firebaseapp.com",
-  projectId: "esdb-4bc35",
-  storageBucket: "esdb-4bc35.firebasestorage.app",
-  messagingSenderId: "693041236078",
-  appId: "1:693041236078:web:6464c0fe14005ea489fe7f",
-  measurementId: "G-Z5PKKM5HCH"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
